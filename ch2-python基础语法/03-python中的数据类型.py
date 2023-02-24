2623de47eecda44165ff485bb607d25b3cbf6acf
ddrEricNo1 <ericDing1811@outlook.com>
2023-02-24 15:25:18 +0800

    second commit

---
+++ ch2-python基础语法/03-python中的数据类型.py
@@ -0,0 +1,10 @@
+print(type(14))
+print(type(1.4))
+print(type("hello"))
+
+name = "黑马程序员"
+name_type = type(name)
+name_type_type = type(name_type)
+print(name_type)
+print(name_type_type)
+print(type(int))
